027cf9f4f670957222fd2b40ef8f541fa3df760d
Faithfulness Alamu <42256651+dents-shape@users.noreply.github.com>
2025-06-26 21:46:23 +0100

    lipgloss: define colors

---
+++ lipgloss/colour.go
@@ -0,0 +1,11 @@
+package lipgloss
+
+import "github.com/charmbracelet/lipgloss/v2"
+
+var (
+	ColourAccent   = lipgloss.Color("#5fffaf")
+	ColourBlack    = lipgloss.Color("#0D0D0D")
+	ColourDarkGray = lipgloss.Color("#404040")
+	ColourGray     = lipgloss.Color("#8C8C8C")
+	ColourWhite    = lipgloss.Color("#F2F2F2")
+)
--- lipgloss/lipgloss.go
+++
@@ -1,3 +0,0 @@
-package lipgloss
-
-type LipGloss struct{}
